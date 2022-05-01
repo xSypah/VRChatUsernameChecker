da43638318716b7aac6193641281a51467336aad
Sypherr <96305817+xSypah@users.noreply.github.com>
2022-05-01 16:56:10 +0200

    Added Files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using VRChat_NameChecker.Utils;
+
+namespace VRChat_NameChecker
+{
+    internal class Program
+    {
+        private static List<string> userNames = File.ReadAllLines("usernames.txt").ToList();
+        private static WebClient webClient = new WebClient();
+        private static bool isAvailable(string username)
+        {
+            webClient.Headers.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36");
+            dynamic response = JsonConvert.DeserializeObject(webClient.DownloadString($"https://vrchat.com/api/1/auth/exists?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26&username={username}"));
+            return response.userExists;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Title = $"KΛRMΛ | VRChat Username Checker | Checking {userNames.Count} Usernames | By Sypherr#3000" ;
+            Logger.Log("Thanks for using our software! discord.gg/karmavrc", Logger.Type.Info);
+            foreach (var username in userNames)
+            {
+                bool userExists = isAvailable(username);
+                if (userExists)
+                {
+                    Logger.Log($"{username} has already been taken!", Logger.Type.Error);
+                    Logger.WriteToFile(username, "unavailable.txt");
+                }
+                else
+                {
+                    Logger.Log($"{username} is available!", Logger.Type.Success);
+                    Logger.WriteToFile(username, "available.txt");
+                }
+                Task.Delay(500).Wait(); // 429
+            }
+            Logger.Log($"Successfully checked all {userNames.Count} usernames! :)");
+            Task.Delay(-1).Wait();
+        }
+    }
+}
---
+++ Utils/Logger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRChat_NameChecker.Utils
+{
+    internal class Logger
+    {
+        public enum Type
+        {
+            Info,
+            Error,
+            Warn,
+            Success,
+        }
+        public static void Log(object Object, Type LogType = Type.Info)
+        {
+            switch (LogType)
+            {
+                case Type.Info:
+                    Write(ConsoleColor.Yellow, "Info", Object.ToString());
+                    break;
+                case Type.Error:
+                    Write(ConsoleColor.Red, "Error", Object.ToString());
+                    break;
+                case Type.Warn:
+                    Write(ConsoleColor.Yellow, "Warn", Object.ToString());
+                    break;
+                case Type.Success:
+                    Write(ConsoleColor.Green, "Success", Object.ToString());
+                    break;
+
+            }
+        }
+
+        private static void Write(ConsoleColor TypeColor, string Type, string Text)
+        {
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("[");
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.Write(DateTime.Now.ToString("HH:mm:ss"));
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("] ");
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("[");
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.Write("KARMA");
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("] ");
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("[");
+            System.Console.ForegroundColor = TypeColor;
+            System.Console.Write(Type);
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("] ");
+            System.Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.WriteLine(Text);
+        }
+
+        public static void WriteToFile(string text, string path)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(text);
+                sw.Close();
+            }
+        }
+    }
+}
